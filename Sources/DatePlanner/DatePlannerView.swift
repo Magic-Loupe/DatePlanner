391505971f0f1546c8d8078fec492f822660ebd9
Abe White <abewhite@mac.com>
2023-01-05 16:16:03 -0600

    Integrate into MagicLoupe

--- Sources/DatePlanner/DatePlannerView.swift
+++ Sources/DatePlanner/DatePlannerView.swift
@@ -3,13 +3,17 @@ import JXSwiftUI
 import SwiftUI
 
 public struct DatePlannerView: View {
+    @StateObject private var eventData = EventData()
+
     public init() {
     }
     
     public var body: some View {
-        JXView { context in
-            try context.registry.register(DatePlannerModule())
-            return try context.new("dateplanner.ContentView")
-        }
+//        JXView { context in
+//            try context.registry.register(DatePlannerModule())
+//            return try context.new("dateplanner.ContentView")
+//        }
+        EventList()
+            .environmentObject(eventData)
     }
 }
